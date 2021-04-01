45bfae786f68c9452fb4d4673a2b7f9397321068
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-03-31 20:53:59 -0500

    created user-routes

---
+++ controllers/api/restaurant-routes.js
@@ -0,0 +1,136 @@
+const router = require('express').Router();
+const { Restaurant, User, Review } = require("../../models");
+const withAuth = require('../../utils/auth');
+
+
+
+// get all restaurants
+router.get('/', (req, res) => {
+  Restaurant.findAll({
+    order: [['created_at', 'DESC']],
+    include: [
+      // include reviews from each restaurant
+      {
+        model: Review,
+        attributes: ['id', 'res_reviewed', 'review_content', 'review_approval'],
+        include: {
+          model: User,
+          attributes: ['displayname, firstname, lastname, email']
+        }
+      },
+    //   include user
+      {
+        model: User,
+        attributes: ['displayname, firstname, lastname, email']
+    }
+    ]
+   })
+   .then(restData => res.json(restData))
+   .catch(err => {
+     console.log(err);
+     res.status(500).json(err);
+   });
+});
+
+// Find a specific restaurant
+router.get('/:id', (req, res) => {
+    Restaurant.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [
+          //include reviews for restaurant
+        {
+            model: Review,
+            attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+            include: {
+              model: User,
+              attributes: ['displayname, firstname, lastname, email']
+            }
+        },
+        //   include user
+        {
+          model: User,
+          attributes: ['displayname, firstname, lastname, email']
+        }
+      ]
+    })
+      .then(restData => {
+        if (!restData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(restData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+
+// create restaurant
+router.post('/', withAuth, (req, res) => {
+
+    Restaurant.create({
+        res_name: req.body.res_name,
+        res_phone: req.body.res_phone,
+        res_website: req.body.res_website,
+        res_address: req.body.res_address,
+        food_style: req.body.food_style,
+        brick_mortar: req.body.brick_mortar,
+        trailer: req.body.trailer,
+        delivery: req.body.delivery,
+        takeout_curbside: req.body.takeout_curbside,
+        reservations: req.body.reservations,
+        on_site_parking: req.body.on_site_parking,
+    })
+      .then(restData => res.json(restData))
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// update restaurant (REVIEW)
+router.put('/:id', withAuth , (req, res) => {
+    Restaurant.update(req.body, {
+        individualHooks: true,
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(restData => {
+        if (!restData) {
+          res.status(404).json({ message: 'No restaurant found with this id' });
+          return;
+        }
+        res.json(restData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+});
+
+// delete restaurant
+router.delete('/:id', withAuth , (req, res) => {
+    Post.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+      .then(restData => {
+        if (!restData) {
+          res.status(404).json({ message: 'No restaurant found with this id' });
+          return;
+        }
+        res.json(restData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
+module.exports = router;
