b03d5b71e1c2eecd0cdbfd4f844c099b8b729edf
jnunez1229 <70110259+jnunez1229@users.noreply.github.com>
2021-04-03 11:04:56 -0500

    created home-routes/created homepage.handlebars

---
+++ controllers/home-routes.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Restaurant, User, Review} = require('../models');
+
+router.get('/', (req, res) => {
+    Restaurant.findAll()
+    .then(homeData => {
+        const posts = homeData.map(post => post.get({ plain: true }));
+        res.render('homepage');
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+  module.exports = router;
