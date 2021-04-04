35c563e7894d80119d68630401455d1955bf6cba
DragoonKite <frank.depaolo@yahoo.com>
2021-04-04 15:56:29 -0500

    update edit-restaurant js

--- public/javascript/add-restaurant.js
+++ public/javascript/add-restaurant.js
@@ -1,32 +1,32 @@
 async function newRestaurantHandler(event) {
     event.preventDefault();
 
-    const resName = document.querySelector('input[name="restaurant-name"]').value;
-    const resPhone = document.querySelector('input[name="restaurant-phone"]').value;
-    const resWebSite = document.querySelector('input[name="restaurant-site"]').value;
-    const resAddress = document.querySelector('input[name="restaurant-address"]').value;
-    const foodStyle = document.querySelector('input[name="food-style"]').value;
-    const brickMortar = document.querySelector('input[name="brick-mortar"]').checked;
+    const res_name = document.querySelector('input[name="restaurant-name"]').value;
+    const res_phone = document.querySelector('input[name="restaurant-phone"]').value;
+    const res_website = document.querySelector('input[name="restaurant-site"]').value;
+    const res_address = document.querySelector('input[name="restaurant-address"]').value;
+    const food_style = document.querySelector('input[name="food-style"]').value;
+    const brick_mortar = document.querySelector('input[name="brick-mortar"]').checked;
     const trailer = document.querySelector('input[name="trailer"]').checked;
     const delivery = document.querySelector('input[name="delivery"]').checked;
-    const takeoutCurbside = document.querySelector('input[name="takeout-curbside"]').checked;
+    const takeout_curbside = document.querySelector('input[name="takeout-curbside"]').checked;
     const reservations = document.querySelector('input[name="reservations"]').checked;
-    const onSiteParking = document.querySelector('input[name="on-site-parking"]').checked;
+    const on_site_parking = document.querySelector('input[name="on-site-parking"]').checked;
 
     const response = await fetch(`/api/restaurant`, {
         method: 'POST',
         body: JSON.stringify({
-        resName,
-        resPhone,
-        resWebSite,
-        resAddress,
-        foodStyle,
-        brickMortar,
+        res_name,
+        res_phone,
+        res_website,
+        res_address,
+        food_style,
+        brick_mortar,
         trailer,
         delivery,
-        takeoutCurbside,
+        takeout_curbside,
         reservations,
-        onSiteParking
+        on_site_parking
         }),
         headers: {
         'Content-Type': 'application/json'
