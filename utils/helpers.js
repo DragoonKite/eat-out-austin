2f2caff61efcd378f1456cc8eaf6631fc0b3a2b3
Mon <rocketorangemen@gmail.com>
2021-04-08 19:38:42 -0500

    card fixes and food style above restaurant

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,6 +1,12 @@
 module.exports = {
     format_name: name => {
         return name.replace(/\s/g, '-')
-    }
+    },
 
+    format_phone: phone => {
+        phone=phone.substring(0, 3)+ '-' + phone.substring(3, 6) + '-' + phone.substring(6, phone.length);
+        return phone;
+    }
 }
+
+
